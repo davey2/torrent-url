a87d7726ea9c4c2976231f99cd1ef6575503753c
David Brutoczki <bcz.david99@gmail.com>
2021-07-11 10:54:46 +0200

    feat: check torrent exist in torrents before add again

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import WebTorrent, { TorrentOptions } from "webtorrent";
+import WebTorrent, { TorrentOptions, Torrent } from "webtorrent";
 import axios from "axios";
+
+declare module "webtorrent" {
+	export interface Torrent {
+		urlList: string[];
+	}
+}
 
 class TorrentURL {
 	private client: WebTorrent.Instance = new WebTorrent();
@@ -10,40 +16,65 @@ class TorrentURL {
 
 	fetch(url: string): Promise<Response> {
 		return new Promise((resolve, reject) => {
-			this.client
-				.add(`${this.indexURL}/${url}`, torrent => {
-					console.log("torrent added", torrent);
-
+			const torrent: Torrent | undefined = this.client.torrents.find(
+				({ urlList }) => urlList.some(item => item === url)
+			);
+			if (torrent) {
+				if (torrent.done) {
+					torrent.files[0].getBlob((error, blob) => {
+						if (error) reject(error);
+						else if (blob) resolve(new Response(blob));
+						else reject();
+					});
+				} else {
 					torrent.on("done", () => {
-						console.log("torrent done");
 						torrent.files[0].getBlob((error, blob) => {
 							if (error) reject(error);
 							else if (blob) resolve(new Response(blob));
 							else reject();
 						});
-
-						torrent.on("noPeers", announceType => {
-							console.log("noPeers", announceType);
-						});
 					});
-				})
-				.on("error", () => {
-					fetch(url)
-						.then(response => {
-							resolve(response);
+				}
+			} else {
+				this.client
+					.add(`${this.indexURL}/${url}`, torrent => {
+						console.log("torrent added", torrent);
 
-							response.blob().then(blob => {
-								this.client.seed(
-									<File>blob,
-									<TorrentOptions>{ name: "webtorrent-url-fetch" },
-									torrent => {
-										this.registerTorrent(url, torrent.torrentFile);
-									}
-								);
+						torrent.on("done", () => {
+							console.log("torrent done");
+							torrent.files[0].getBlob((error, blob) => {
+								if (error) reject(error);
+								else if (blob) resolve(new Response(blob));
+								else reject();
 							});
-						})
-						.catch(reject);
-				});
+
+							torrent.on("noPeers", announceType => {
+								console.log("noPeers", announceType);
+							});
+						});
+					})
+					.on("error", () => {
+						fetch(url)
+							.then(response => {
+								resolve(response);
+
+								response.blob().then(blob => {
+									this.client.seed(
+										<File>blob,
+										<TorrentOptions>{
+											name: "webtorrent-url-fetch",
+											urlList: [url]
+										},
+										torrent => {
+											this.registerTorrent(url, torrent.torrentFile);
+											console.log(torrent);
+										}
+									);
+								});
+							})
+							.catch(reject);
+					});
+			}
 		});
 	}
 
